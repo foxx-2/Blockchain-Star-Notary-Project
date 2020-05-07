0c1e8a6de31b435e4c4e412e9768a5214e509a51
Noman Nasir Minhas <sp17bsse010@gmail.com>
2020-05-07 23:15:30 +0500

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Blockchain-Star-Notary-Project
 This project will be used to deploy smart contract on Rinkeby Test Network to manage and exchange information about stars.
+
+# ERC-721 Token Name
+## CheckmateToken
+
+# ERC-721 Token Symbol
+## CKMT
+
+# Version of the Truffle 
+## Truffle v5.1.24
+
+# Version of 
+## openzeppelin-solidity, "version": 2.1.2
+
+# Task 1
+- Add a name and a symbol for your starNotary tokens.
+- Add a function lookUptokenIdToStarInfo, that looks up the stars using the Token ID, and then returns the name of the star.
+- Add a function called exchangeStars, so 2 users can exchange their star tokens...Do not worry about the price, just write code to exchange stars between users.
+- Write a function to Transfer a Star. The function should transfer a star from the address of the caller. The function should accept 2 arguments, the address to transfer the star to, and the token ID of the star.
+
+# Task 2
+- Add supporting unit tests, to test the following:
+1. The token name and token symbol are added properly.
+2. 2 users can exchange their stars.
+3. Stars Tokens can be transferred from one address to another.
+
+# Task 3
+##Deploy your Contract to Rinkeby
+
+- Edit the truffle.config file to add settings to deploy your contract to the Rinkeby Public Network.
+- Helper Points:
+1. Command used to deploy to Rinkeby truffle migrate --reset --network rinkeby
+2. You will need to have your Metamask’s seed and Infura setup.
+3. This was shown to you in detail in the lesson on Solidity, while creating ERC-20 tokens on Rinkeby.
+
+# Task 4
+- Modify the front end of the DAPP to achieve the following:
+- Lookup a star by ID using tokenIdToStarInfo() (you will have to add code for this in your index.html and index.js files)
+
+
---
+++ app/src/index.js
@@ -0,0 +1,79 @@
+import starNotaryArtifact from "../../build/contracts/StarNotary.json";
+import Web3 from "web3";
+
+const App = {
+  web3: null,
+  account: null,
+  meta: null,
+
+  start: async function() {
+    const { web3 } = this;
+
+    try {
+      // get contract instance
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = starNotaryArtifact.networks[networkId];
+      this.meta = new web3.eth.Contract(
+        starNotaryArtifact.abi,
+        deployedNetwork.address,
+      );
+
+      // get accounts
+      const accounts = await web3.eth.getAccounts();
+      this.account = accounts[0];
+    } catch (error) {
+      console.error("Could not connect to contract or chain.");
+    }
+  },
+
+  setStatus: function(message) {
+    const status = document.getElementById("status");
+    status.innerHTML = message;
+  },
+
+  createStar: async function() {
+    const { createStar } = this.meta.methods;
+    const name = document.getElementById("starName").value;
+    const id = document.getElementById("starId").value;
+    await createStar(name, id).send({from: this.account});
+    App.setStatus("New Star Owner is " + this.account + ".");
+  },
+
+  // Implement Task 4 Modify the front end of the DAPP
+  lookUp: async function (){
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+          console.log(error);
+          return
+      }
+
+      const account = accounts[0];
+      const tokenIdDetails = document.getElementById("tokenIdDetails").value;
+
+      starNotary.tokenIdToStarInfo(tokenIdDetails, {from: account, gas: 3000000}, function (error, result) {
+          if (error) {
+              document.getElementById("status").innerHTML = error;
+          } else {
+              document.getElementById("status").innerHTML = result;
+          }
+      })
+  })
+  }
+
+};
+
+window.App = App;
+
+window.addEventListener("load", async function() {
+  if (window.ethereum) {
+    // use MetaMask's provider
+    App.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable(); // get permission to access accounts
+  } else {
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+  }
+
+  App.start();
+});
